7abbbe6fa1909d948616c6665bae065fcaf3e1b6
enrique95 <enrique_alvarado95@hotmail.com>
2022-06-06 12:26:00 -0500

    se  agrega componente de Layout y navbar

---
+++ components/Layout.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Head from 'next/head';
+import Navbar from './Navbar';
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Launchy</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar />
+      {children}
+    </>
+  );
+};
+
+export default Layout;
---
+++ components/Navbar.jsx
@@ -0,0 +1,63 @@
+import Link from 'next/link';
+import React from 'react';
+import Image from 'next/image';
+import Logo from '../public/img/logo.png';
+const Links = [
+  {
+    title: 'soluciones',
+    src: '/',
+  },
+  {
+    title: 'proyectos',
+    src: '/',
+  },
+  {
+    title: 'cultura',
+    src: '/',
+  },
+  {
+    title: 'blog',
+    src: '/',
+  },
+  {
+    title: 'free resources',
+    src: '/',
+  },
+];
+
+const Navbar = () => {
+  return (
+    <nav className="w-full h-[70px] bg-black">
+      <div className="max-w-[1420px]  h-full mx-auto flex justify-between items-center">
+        {/*image container*/}
+        <div className=" cursor-pointer ">
+          <Image
+            src="/img/logo.png"
+            alt="Launchy Logo"
+            width="140px"
+            height="40px"
+          />
+        </div>
+        {/*links container*/}
+        <div className="flex space-x-4 ">
+          {Links.map((l) => (
+            <p
+              className="text-white uppercase text-sm cursor-pointer"
+              key={l.title}
+            >
+              {l.title}
+            </p>
+          ))}
+        </div>
+        {/*button */}
+        <div>
+          <button className="text-white py-2 px-3 uppercase text-sm navbar_button">
+            Charlemos
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
