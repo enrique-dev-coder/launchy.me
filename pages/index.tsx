0d2bf67aa79b4fa567cdbd82ffcbe38a64f7e3e1
enrique95 <enrique_alvarado95@hotmail.com>
2022-06-09 13:12:54 -0500

    se agrega sliders de logos a home

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import StarBackground from '../components/Wrappers/StarBackground';
 import ParteDosHome from '../components/Parte2Home/index';
 import ParteTresHome from '../components/Parte3Home/index';
 import ParteCuatroHome from '../components/Parte4Home/index';
-
+import LogoSlider from '../components/LogoSlider';
 const Home: NextPage = () => {
   return (
     <div>
@@ -13,6 +13,7 @@ const Home: NextPage = () => {
         <ParteDosHome />
         <ParteTresHome />
         <ParteCuatroHome />
+        <LogoSlider />
       </StarBackground>
     </div>
   );
