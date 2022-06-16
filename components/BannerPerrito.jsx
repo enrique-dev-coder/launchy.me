179aaf7c7c69c046a1478cbec8ad11e422306f15
enrique95 <enrique_alvarado95@hotmail.com>
2022-06-15 21:28:30 -0500

    banners adpatados a responsive

--- components/BannerPerrito.jsx
+++ components/BannerPerrito.jsx
@@ -8,12 +8,12 @@ const BannerPerrito = () => {
         <div className="w-full h-[60%] ">
           <div className="w-full flex justify-center mb-[10px]">
             <img src="/img/huella.png" />
-            <h2 className="text-white uppercase text-3xl roboto_condesed font-bold mx-4">
+            <h2 className="text-white uppercase md:text-3xl roboto_condesed font-bold mx-4">
               ¡QUEREMOS DAR UNA PATA!
             </h2>
             <img src="/img/huella.png" className="  rotate-180" />
           </div>
-          <p className="text-white text-center w-[40%] mx-auto mb-[20px]">
+          <p className="text-white text-center w-4/5 md:w-[40%] mx-auto mb-[20px]">
             El 5% de nuestro profit se destina a ayudar al rescate de animales y
             su adopción.
           </p>
