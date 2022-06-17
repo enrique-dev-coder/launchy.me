f8e17a93a174ef0d82da50c2fd46d315d0ffab0f
enrique95 <enrique_alvarado95@hotmail.com>
2022-06-17 11:51:04 -0500

    prueba de instalacion de GA

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -3,10 +3,25 @@ import Head from 'next/head';
 import Navbar from './Navbar';
 import { useMenu } from '../context/MenuContext';
 import MobileMenu from './MobileMenu';
+import Script from 'next/script';
 const Layout = ({ children }) => {
   const { isOpen, setisOpen } = useMenu();
   return (
     <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script strategy="lazyOnload">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
       <Head>
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
