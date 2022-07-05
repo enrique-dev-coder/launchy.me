471f0ce0b8dcd49ba997b5ff4e51141e67468c07
enrique95 <enrique_alvarado95@hotmail.com>
2022-07-05 14:14:28 -0500

    se arregla error en una ruta

--- pages/culture/index.jsx
+++ pages/culture/index.jsx
@@ -1,27 +1,29 @@
-import Header from '../../components/pagesAtom/Header'
-import Mission from '../../components/pagesAtom/Culture/Mission'
-import Cards from '../../components/pagesAtom/culture/Cards'
-import TeamPictures from '../../components/pagesAtom/Culture/TeamPictures'
-import Together from '../../components/pagesAtom/Culture/Together'
-import StarBackgroundCss from '../../components/Wrappers/StarBackgroundCss'
-import Footer from '../../components/Footer'
+import Header from '../../components/pagesAtom/Header';
+import Mission from '../../components/pagesAtom/Culture/Mission';
+import Cards from '../../components/pagesAtom/Culture/Cards';
+import TeamPictures from '../../components/pagesAtom/Culture/TeamPictures';
+import Together from '../../components/pagesAtom/Culture/Together';
+import StarBackgroundCss from '../../components/Wrappers/StarBackgroundCss';
+import Footer from '../../components/Footer';
 
 const Culture = () => {
   return (
     <main className="2xl:container 2xl:mx-auto roboto_normal">
-        <Header show={false}>
-            <h2 className='z-10 text-white tracking-wider uppercase mt-5 text-[30px]'>Culture</h2>
-        </Header>
+      <Header show={false}>
+        <h2 className="z-10 text-white tracking-wider uppercase mt-5 text-[30px]">
+          Culture
+        </h2>
+      </Header>
 
-        <StarBackgroundCss>
-            <Cards />
-            <TeamPictures />
-            <Mission />
-            <Together />
-            <Footer />
-        </StarBackgroundCss>
+      <StarBackgroundCss>
+        <Cards />
+        <TeamPictures />
+        <Mission />
+        <Together />
+        <Footer />
+      </StarBackgroundCss>
     </main>
-  )
-}
+  );
+};
 
-export default Culture
+export default Culture;
