1e3cf2c988f9603b2e0cfdb1cb9aa4f54618155c
enrique95 <enrique_alvarado95@hotmail.com>
2022-07-13 12:23:47 -0500

    se agrega animacion en json a cultutre

--- components/pagesAtom/servicios/CJMPicture.jsx
+++ components/pagesAtom/servicios/CJMPicture.jsx
@@ -1,32 +1,69 @@
-import React from 'react'
-import Wrapper from '../Wrapper'
-
+import React from 'react';
+import Wrapper from '../Wrapper';
+import Lottie from 'react-lottie';
+import { circulo } from '../../../public/animation.json';
 const CJMPicture = () => {
+  const LottieOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: circulo,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
   return (
     <Wrapper>
-      <div className='mb-[40px] md:mb-[60px] relative '>
-          <div className="bg-[url(/img/Fondo.png)] bg-cover flex flex-col lg:flex-row lg:items-center p-[20px] gap-4 max-w-full">
-                  <img src="/img/atomo-animation.gif" alt="" className="w-[150px] h-[150px] sm:w-[300px] sm:h-[300px] mx-auto relative z-20" />
-
-                  <div className="">
-                      <p className="text-[#211539] text-left relative z-20 text-[14px] lg:text-[12px] lg:font-medium mb-[6px]">Es por eso que desde Launchy identificamos cada aspecto de tu CJM (Customer Journey Map). Involucrando desde tu propuesta de valor, buyer persona, competencias, partners, marketing inbound area, marketing outbound area, area comercial y el area mas Underrated “Customer success area”. Con el fin de encontrar esos accionables, tácticas o estrategias que puedan dar un giro 180° a tus indicadores de Revenue &amp; Profit.</p>
-                      <h2 className="font-bold text-2xl text-left text-[#211539] relative z-20 lg:text-[18px] mb-[6px]">Nuestras consultorias principales son:</h2>
-                      <ul className="flex flex-col items-start relative z-20">
-                          <li className="text-[#211539] text-[14px] lg:text-[12px] lg:font-medium">- Auditoria Customer Journey Map</li>
-                          <li className="text-[#211539] text-[14px] lg:text-[12px] lg:font-medium">- Optimization B2B Outbound marketing Funnels</li>
-                          <li className="text-[#211539] text-[14px] lg:text-[12px] lg:font-medium">- Optimization B2B Inbound Marketing Funnels</li>
-                          <li className="text-[#211539] text-[14px] lg:text-[12px] lg:font-medium">- Optimization B2B Sales Funnels</li>
-                          <li className="text-[#211539] text-[14px] lg:text-[12px] lg:font-medium">- Optimization B2B Partners Funnels</li>
-                          <li className="text-[#211539] text-[14px] lg:text-[12px] lg:font-medium">- Optimization B2B Customer Success Funnel</li>
-                          <li className="text-[#211539] text-[14px] lg:text-[12px] lg:font-medium">- Optimization Semantic y Technical SEO</li>
-                          <li className="text-[#211539] text-[14px] lg:text-[12px] lg:font-medium">- Website Optimization Conversion Rate</li>
-                      </ul>
-                  </div>
+      <div className="mb-[40px] md:mb-[60px] relative ">
+        <div className="bg-[url(/img/Fondo.png)] bg-cover flex flex-col lg:flex-row lg:items-center p-[20px] gap-4 max-w-full">
+          <div>
+            <Lottie options={LottieOptions} height={300} width={300} />
           </div>
-          <div className="team-background w-full h-full absolute z-10 top-0 left-0 bottom-0"></div>
+
+          <div className="">
+            <p className="text-[#211539] text-left relative z-20 text-[14px] lg:text-[12px] lg:font-medium mb-[6px]">
+              Es por eso que desde Launchy identificamos cada aspecto de tu CJM
+              (Customer Journey Map). Involucrando desde tu propuesta de valor,
+              buyer persona, competencias, partners, marketing inbound area,
+              marketing outbound area, area comercial y el area mas Underrated
+              “Customer success area”. Con el fin de encontrar esos accionables,
+              tácticas o estrategias que puedan dar un giro 180° a tus
+              indicadores de Revenue &amp; Profit.
+            </p>
+            <h2 className="font-bold text-2xl text-left text-[#211539] relative z-20 lg:text-[18px] mb-[6px]">
+              Nuestras consultorias principales son:
+            </h2>
+            <ul className="flex flex-col items-start relative z-20">
+              <li className="text-[#211539] text-[14px] lg:text-[12px] lg:font-medium">
+                - Auditoria Customer Journey Map
+              </li>
+              <li className="text-[#211539] text-[14px] lg:text-[12px] lg:font-medium">
+                - Optimization B2B Outbound marketing Funnels
+              </li>
+              <li className="text-[#211539] text-[14px] lg:text-[12px] lg:font-medium">
+                - Optimization B2B Inbound Marketing Funnels
+              </li>
+              <li className="text-[#211539] text-[14px] lg:text-[12px] lg:font-medium">
+                - Optimization B2B Sales Funnels
+              </li>
+              <li className="text-[#211539] text-[14px] lg:text-[12px] lg:font-medium">
+                - Optimization B2B Partners Funnels
+              </li>
+              <li className="text-[#211539] text-[14px] lg:text-[12px] lg:font-medium">
+                - Optimization B2B Customer Success Funnel
+              </li>
+              <li className="text-[#211539] text-[14px] lg:text-[12px] lg:font-medium">
+                - Optimization Semantic y Technical SEO
+              </li>
+              <li className="text-[#211539] text-[14px] lg:text-[12px] lg:font-medium">
+                - Website Optimization Conversion Rate
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="team-background w-full h-full absolute z-10 top-0 left-0 bottom-0"></div>
       </div>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default CJMPicture
+export default CJMPicture;
--- pages/services/index.jsx
+++ pages/services/index.jsx
@@ -1,143 +1,212 @@
-import Header from '../../components/pagesAtom/Header'
-import StarBackgroundCss from '../../components/Wrappers/StarBackgroundCss'
-import ServicesText from '../../components/pagesAtom/servicios/ServicesText'
-import Cases from '../../components/pagesAtom/servicios/Cases'
-import Circle from '../../components/pagesAtom/servicios/Circle'
-import StackSlider from '../../components/pagesAtom/servicios/StackSlider'
-import OtherCases from '../../components/pagesAtom/servicios/OtherCases'
-import Pictures from '../../components/pagesAtom/servicios/Pictures'
-import CJMPicture from '../../components/pagesAtom/servicios/CJMPicture'
-import HeaderSlider from '../../components/pagesAtom/HeaderSlider'
-import FlippingCard from '../../components/pagesAtom/servicios/FlippingCard'
-import Wrapper from '../../components/pagesAtom/Wrapper'
-import Footer from '../../components/Footer'
+import Header from '../../components/pagesAtom/Header';
+import StarBackgroundCss from '../../components/Wrappers/StarBackgroundCss';
+import ServicesText from '../../components/pagesAtom/servicios/ServicesText';
+import Cases from '../../components/pagesAtom/servicios/Cases';
+import Circle from '../../components/pagesAtom/servicios/Circle';
+import StackSlider from '../../components/pagesAtom/servicios/StackSlider';
+import OtherCases from '../../components/pagesAtom/servicios/OtherCases';
+import Pictures from '../../components/pagesAtom/servicios/Pictures';
+import CJMPicture from '../../components/pagesAtom/servicios/CJMPicture';
+import HeaderSlider from '../../components/pagesAtom/HeaderSlider';
+import FlippingCard from '../../components/pagesAtom/servicios/FlippingCard';
+import Wrapper from '../../components/pagesAtom/Wrapper';
+import Footer from '../../components/Footer';
 
 // Header items
 const items = [
-    'PROACTIVOS',
-    'ANALITICOS',
-    'MAKERS',
-    'PIZZA',
-    'DREAMERS',
-    'ENTUSIASTAS',
-    'APASIONADOS',
-]
+  'PROACTIVOS',
+  'ANALITICOS',
+  'MAKERS',
+  'PIZZA',
+  'DREAMERS',
+  'ENTUSIASTAS',
+  'APASIONADOS',
+];
 
 // ServicesText texts
 const texts = {
-  research: 'Todo parte de la empatía. No basta con suponer por el usuario, investigamos de manera cualitativa y cuantitativa. Creemos fielmente en que lo que se puede medir se puede mejorar. Buscamos los insights claves, proponemos nuevas rutas de investigación que sumen a tus objetivos. Amamos tener inciativa y poder ofrecerte:',
-  design: 'Diseño con propósito e investigación. Planteamos aplicaciones y productos que sigan una linea visual clara para transmitir la propuesta de valor del cliente. Partimos de identidades creadas o de cero, co creando juntos. Vamos más allá de lo estético brindadote nuestra expertis para aconsejarte y darte opciones con criterio y en base a propósitos que se alineen con tu estrategia de marca. ',
-  develop: 'Ofrecemos soluciones personalizadas adaptandonos al contexto, road map y posibilidades de nuestros clientes. Para ello combinamos metodologias agiles con un equipo multidisciplinario que permite abrir una comunicación fluida estableciendo rutas claras para impulsar tus ideas al espacio.',
-  doBussiness: '¿Has desarrollado un MVP o has escalado un gran producto/servicio? Si es asi seguramente pasaste horas de investigación, desarrollo, tareas operativas y un compromiso por dar a flote tu negocio.',
-}
+  research:
+    'Todo parte de la empatía. No basta con suponer por el usuario, investigamos de manera cualitativa y cuantitativa. Creemos fielmente en que lo que se puede medir se puede mejorar. Buscamos los insights claves, proponemos nuevas rutas de investigación que sumen a tus objetivos. Amamos tener inciativa y poder ofrecerte:',
+  design:
+    'Diseño con propósito e investigación. Planteamos aplicaciones y productos que sigan una linea visual clara para transmitir la propuesta de valor del cliente. Partimos de identidades creadas o de cero, co creando juntos. Vamos más allá de lo estético brindadote nuestra expertis para aconsejarte y darte opciones con criterio y en base a propósitos que se alineen con tu estrategia de marca. ',
+  develop:
+    'Ofrecemos soluciones personalizadas adaptandonos al contexto, road map y posibilidades de nuestros clientes. Para ello combinamos metodologias agiles con un equipo multidisciplinario que permite abrir una comunicación fluida estableciendo rutas claras para impulsar tus ideas al espacio.',
+  doBussiness:
+    '¿Has desarrollado un MVP o has escalado un gran producto/servicio? Si es asi seguramente pasaste horas de investigación, desarrollo, tareas operativas y un compromiso por dar a flote tu negocio.',
+};
 
 //Words Slider
-const words = [
-  'INVESTIGAR',
-  'PROTOTIPAR',
-  'ITERAR',
-  'TESTEAR',
-  'MEJORAR',
-]
+const words = ['INVESTIGAR', 'PROTOTIPAR', 'ITERAR', 'TESTEAR', 'MEJORAR'];
 
 //Circle
 const circles = [
-  {number: 1, text: 'ROAD MAP'},
-  {number: 2, text: 'RECOPILACION DE DATOS'},
-  {number: 3, text: 'Análisis'},
-  {number: 4, text: 'TESTEO'},
-  {number: 5, text: 'Iteración'}
-]
+  { number: 1, text: 'ROAD MAP' },
+  { number: 2, text: 'RECOPILACION DE DATOS' },
+  { number: 3, text: 'Análisis' },
+  { number: 4, text: 'TESTEO' },
+  { number: 5, text: 'Iteración' },
+];
 
 //Flipping Cards
 const flipping = [
-  {number: 1, src: '/img/team/Research.mp4', title: 'research', video: true},
-  {number: 2, src: '/img/team/Diseño.png', title: 'diseño', video: false},
-  {number: 3, src: '/img/team/Desarrollo.png', title: 'desarrollo', video: false},
-  {number: 4, src: '/img/team/Bussiness-consulting.png', title: 'bussiness consulting', video: false},
-]
+  { number: 1, src: '/img/team/Research.mp4', title: 'research', video: true },
+  { number: 2, src: '/img/team/Diseño.png', title: 'diseño', video: false },
+  {
+    number: 3,
+    src: '/img/team/Desarrollo.png',
+    title: 'desarrollo',
+    video: false,
+  },
+  {
+    number: 4,
+    src: '/img/team/Bussiness-consulting.png',
+    title: 'bussiness consulting',
+    video: false,
+  },
+];
 
 const Servicios = () => {
   return (
     <main className="roboto_normal">
-        <Header items={items} quantity={6}>
-            <h2 className='z-10 text-white text-xl sm:text-4xl mt-3'>SERVICIOS: ¿CÓMO EMPEZAMOS?</h2>
-        </Header>
+      <Header items={items} quantity={6}>
+        <h2 className="z-10 text-white text-xl sm:text-4xl mt-3">
+          SERVICIOS: ¿CÓMO EMPEZAMOS?
+        </h2>
+      </Header>
 
-        <StarBackgroundCss>
-          <div className='max-w-[1440px] mx-auto'>
+      <StarBackgroundCss>
+        <div className="max-w-[1440px] mx-auto">
+          <div className="flex items-center lg:justify-center overflow-x-auto scrollbar py-[1rem] px-[6%] gap-6 mt-[60px]">
+            {flipping.map((card) => (
+              <FlippingCard
+                key={card.number}
+                picture={card.src}
+                title={card.title}
+                link={card.title}
+                video={card.video}
+              />
+            ))}
+          </div>
 
-                <div className="flex items-center lg:justify-center overflow-x-auto scrollbar py-[1rem] px-[6%] gap-6 mt-[60px]">
-                {
-                flipping.map(card => <FlippingCard key={card.number} picture={card.src} title={card.title} link={card.title} video={card.video} />)
-                }
-                </div>
+          <ServicesText
+            title="research"
+            text={texts.research}
+            picture="/img/team/We-research.png"
+            id="research"
+          >
+            <ul className="flex flex-col">
+              <li className="text-slate-100 text-[14px]">Product Discovery</li>
+              <li className="text-slate-100 text-[14px]">Benchmaking</li>
+              <li className="text-slate-100 text-[14px]">UX Research</li>
+              <li className="text-slate-100 text-[14px]">
+                Bussiness Model Research
+              </li>
+              <li className="text-slate-100 text-[14px]">
+                Data Analisis Marketing
+              </li>
+            </ul>
+          </ServicesText>
 
-                <ServicesText title='research' text={texts.research} picture='/img/team/We-research.png' id='research'>
-                  <ul className='flex flex-col'>
-                    <li className='text-slate-100 text-[14px]'>Product Discovery</li>
-                    <li className='text-slate-100 text-[14px]'>Benchmaking</li>
-                    <li className='text-slate-100 text-[14px]'>UX Research</li>
-                    <li className='text-slate-100 text-[14px]'>Bussiness Model Research</li>
-                    <li className='text-slate-100 text-[14px]'>Data Analisis Marketing</li>
-                  </ul>
-                </ServicesText>
+          <div className="px-[8%] py-[4%] flex justify-start md:justify-center overflow-x-auto scrollbar">
+            {circles.map((circle) => (
+              <Circle
+                key={circle.number}
+                number={circle.number}
+                text={circle.text}
+              />
+            ))}
+          </div>
 
-                <div className="px-[8%] py-[4%] flex justify-start md:justify-center overflow-x-auto scrollbar">
-                {
-                circles.map(circle => <Circle key={circle.number} number={circle.number} text={circle.text} />)
-                }
-                </div>
+          <ServicesText
+            title="design"
+            text={texts.design}
+            picture="/img/team/We-design.png"
+            id="diseño"
+          >
+            <ul className="flex flex-col">
+              <li className="text-slate-100 text-[14px]">Diseño UX</li>
+              <li className="text-slate-100 text-[14px]">Diseño UI</li>
+              <li className="text-slate-100 text-[14px]">
+                Diseño de experiencias inmersivas
+              </li>
+              <li className="text-slate-100 text-[14px]">
+                Estrategia de marca
+              </li>
+              <li className="text-slate-100 text-[14px]">
+                Aplicaciones de marca con especialidad coorporativa
+              </li>
+              <li className="text-slate-100 text-[14px]">
+                Diseño de animaciones y audiovisual
+              </li>
+            </ul>
+          </ServicesText>
 
-                <ServicesText title='design' text={texts.design} picture='/img/team/We-design.png' id='diseño'>
-                  <ul className='flex flex-col'>
-                    <li className='text-slate-100 text-[14px]'>Diseño UX</li>
-                    <li className='text-slate-100 text-[14px]'>Diseño UI</li>
-                    <li className='text-slate-100 text-[14px]'>Diseño de experiencias inmersivas</li>
-                    <li className='text-slate-100 text-[14px]'>Estrategia de marca</li>
-                    <li className='text-slate-100 text-[14px]'>Aplicaciones de marca con especialidad coorporativa</li>
-                    <li className='text-slate-100 text-[14px]'>Diseño de animaciones y audiovisual</li>
-                  </ul>
-                </ServicesText>
+          <HeaderSlider
+            slidesItems={words}
+            slidesQuantity={2}
+            slidesMobiles={1}
+            styles="max-h-[200px] flex w-full z-10 tracking-wider text-[50px] sm:text-[70px] mb-[60px] md:mb-[80px] resource_text"
+          />
 
-                <HeaderSlider slidesItems={words} slidesQuantity={2} slidesMobiles={1} styles='max-h-[200px] flex w-full z-10 tracking-wider text-[50px] sm:text-[70px] mb-[60px] md:mb-[80px] resource_text' />
+          <OtherCases />
 
-                <OtherCases />
+          <ServicesText
+            title="develop"
+            text={texts.develop}
+            picture="/img/team/We-develop.png"
+            id="desarrollo"
+          >
+            <ul className="flex flex-col">
+              <li className="text-slate-100 text-[14px]">
+                Website app development
+              </li>
+              <li className="text-slate-100 text-[14px]">
+                Frontend speciality
+              </li>
+              <li className="text-slate-100 text-[14px]">
+                3D inmersive experiences
+              </li>
+              <li className="text-slate-100 text-[14px]">
+                Custome software development
+              </li>
+              <li className="text-slate-100 text-[14px]">
+                Web 3.0 development
+              </li>
+            </ul>
+          </ServicesText>
 
-                <ServicesText title='develop' text={texts.develop} picture='/img/team/We-develop.png' id='desarrollo'>
-                  <ul className='flex flex-col'>
-                    <li className='text-slate-100 text-[14px]'>Website app development</li>
-                    <li className='text-slate-100 text-[14px]'>Frontend speciality</li>
-                    <li className='text-slate-100 text-[14px]'>3D inmersive experiences</li>
-                    <li className='text-slate-100 text-[14px]'>Custome software development</li>
-                    <li className='text-slate-100 text-[14px]'>Web 3.0 development</li>
-                  </ul>
-                </ServicesText>
+          <StackSlider />
 
-                <StackSlider />
+          <Cases />
 
-                <Cases />
+          <ServicesText
+            title="do bussiness consulting"
+            text={texts.doBussiness}
+            picture="/img/team/We-do-bussiness.png"
+            id="bussiness consulting"
+          >
+            <ul className="flex flex-col">
+              <li className="text-slate-100 text-[14px]">Product Discovery</li>
+              <li className="text-slate-100 text-[14px]">Benchmaking</li>
+              <li className="text-slate-100 text-[14px]">UX Research</li>
+              <li className="text-slate-100 text-[14px]">
+                Bussiness Model Research
+              </li>
+              <li className="text-slate-100 text-[14px]">
+                Data Analisis Marketing
+              </li>
+            </ul>
+          </ServicesText>
 
-                <ServicesText title='do bussiness consulting' text={texts.doBussiness} picture='/img/team/We-do-bussiness.png' id='bussiness consulting'>
-                  <ul className='flex flex-col'>
-                    <li className='text-slate-100 text-[14px]'>Product Discovery</li>
-                    <li className='text-slate-100 text-[14px]'>Benchmaking</li>
-                    <li className='text-slate-100 text-[14px]'>UX Research</li>
-                    <li className='text-slate-100 text-[14px]'>Bussiness Model Research</li>
-                    <li className='text-slate-100 text-[14px]'>Data Analisis Marketing</li>
-                  </ul>
-                </ServicesText>
+          <Pictures />
 
-                <Pictures />
+          <CJMPicture />
+        </div>
 
-                <CJMPicture />
-
-            </div>
-
-            <Footer />
-        </StarBackgroundCss>
+        <Footer />
+      </StarBackgroundCss>
     </main>
-  )
-}
+  );
+};
 
-export default Servicios
+export default Servicios;
